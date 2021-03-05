48147bfe6e5bb03f67a9d54afad07249831f7401
Gintaras Sova <gintaras_sova@hotmail.com>
2021-03-05 09:32:34 +0000

    Move JS to class

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,54 +1,68 @@
-$(function () {
-    var button = $('#city-submit'),
-        input = $('#city-input'),
-        response = $('#weather-response'),
-        error = $('#weather-error');
+"use strict";
 
-    input.autocomplete({
-        minLength: 3,
-        source: function (object, response) {
-            $.get(
-                '/city',
-                {
-                    city: object.term
-                }
-            ).done(function (data) {
-                response($.map(data, function (item) {
-                    return {
-                        label: item.name + ',' + item.country,
-                        value: item.name + ',' + item.country
-                    };
-                }));
-            });
-        },
-        select: function () {
-            button.prop('disabled', false);
-        }
-    });
+class Wheather {
+    constructor() {
+        this.button = $('#city-submit');
+        this.input = $('#city-input');
+        this.response = $('#weather-response');
+        this.error = $('#weather-error');
+    }
 
-    button.click(function () {
-        $(this).prop('disabled', true);
+    init() {
+        this.autocomplete();
+        this.send();
+    };
 
-        $.get(
-            '/weather',
-            {
-                location: input.val()
+    autocomplete() {
+        this.input.autocomplete({
+            minLength: 3,
+            source: (object, response) => {
+                $.get(
+                    '/city',
+                    {
+                        city: object.term
+                    }
+                ).done((data) => {
+                    response($.map(data, (item) => {
+                        return {
+                            label: item.name + ',' + item.country,
+                            value: item.name + ',' + item.country
+                        };
+                    }));
+                });
             }
-        ).done(function (data) {
-            console.log(data);
-            if (data.error !== undefined) {
-                error.find('.card-body').text(data.error);
-            } else {
-                response.find('.card-body').html(
-                    `<p>Feels like: <span class="badge badge-pill badge-primary">${data.main.feels_like}</span></p>
-                    <p>Humidity: <span class="badge badge-pill badge-primary">${data.main.humidity}</span></p>
-                    <p>Pressure: <span class="badge badge-pill badge-primary">${data.main.pressure}</span></p>
-                    <p>Temperature: <span class="badge badge-pill badge-primary">${data.main.temp}</span></p>`
-                );
-            }
-
-            error.toggleClass('d-none', data.error === undefined);
-            response.toggleClass('d-none', data.error !== undefined);
         });
-    });
+    };
+
+    send() {
+        this.button.click(() => {
+            $(this).prop('disabled', true);
+
+            $.get(
+                '/weather',
+                {
+                    location: this.input.val()
+                }
+            ).done((data) => {
+                if (data.error !== undefined) {
+                    this.error.find('.card-body').text(data.error);
+                } else {
+                    this.response.find('.card-body').html(
+                        `<p>Feels like: <span class="badge badge-pill badge-primary">${data.main.feels_like}</span></p>
+                        <p>Humidity: <span class="badge badge-pill badge-primary">${data.main.humidity}</span></p>
+                        <p>Pressure: <span class="badge badge-pill badge-primary">${data.main.pressure}</span></p>
+                        <p>Temperature: <span class="badge badge-pill badge-primary">${data.main.temp}</span></p>`
+                    );
+                }
+
+                this.error.toggleClass('d-none', data.error === undefined);
+                this.response.toggleClass('d-none', data.error !== undefined);
+                $(this).prop('disabled', false);
+            });
+        });
+    };
+}
+
+$(function () {
+    (new Wheather()).init();
 });
