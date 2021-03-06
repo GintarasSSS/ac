194038665562dfb988ba52ea45f09f3212ee746a
Gintaras Sova <gintaras_sova@hotmail.com>
2021-03-06 09:52:28 +0000

    Run initial commands in install file

--- install.sh
+++ install.sh
@@ -11,3 +11,8 @@ fi
 docker-compose up -d --build php mysql
 
 docker-compose run --rm php bash -c "chown -R www-data:www-data ./*"
+
+# running in case it wasn't run - todo: check why
+docker-compose run --rm composer install
+# running migration
+docker-compose run --rm artisan migrate
